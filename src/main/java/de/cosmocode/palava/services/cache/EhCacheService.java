d590dd2beb65ad7ed00e7c93ae6bbc6871482726
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-01-27 12:13:00 +0000

    refactored palava packages

--- src/main/java/de/cosmocode/palava/services/cache/EhCacheService.java
+++ src/main/java/de/cosmocode/palava/services/cache/EhCacheService.java
@@ -34,8 +34,8 @@ import org.slf4j.LoggerFactory;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
-import de.cosmocode.palava.core.lifecycle.Initializable;
-import de.cosmocode.palava.core.lifecycle.Startable;
+import de.cosmocode.palava.core.framework.lifecycle.Initializable;
+import de.cosmocode.palava.core.framework.lifecycle.Startable;
 
 /**
  * An implementation of the {@link CacheService} interface
